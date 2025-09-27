c55cd95623b50bae7d085cd459e729233ab20288
Mased <115732111+Masedzik@users.noreply.github.com>
2025-09-27 22:40:26 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿using Discord;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
-using Microsoft.Extensions.DependencyInjection;
+
 using BugReportBot.EventHandlers;
 using BugReportBot.Configs;
 using BugReportBot.Modules;
@@ -10,39 +13,32 @@ namespace BugReportBot
 {
     class Program
     {
-        public static readonly ConfigReader Config = new ConfigReader();
-        public static System.Timers.Timer Interval = new System.Timers.Timer();
-        public static int ?Id;
-
+        private static Config? _config;
         private DiscordSocketClient? _client;
         private static readonly DiscordSocketConfig _socketConfig = new()
         {
-            GatewayIntents = GatewayIntents.Guilds,
-            AlwaysDownloadUsers = true,
+            GatewayIntents = GatewayIntents.Guilds
         };
+
+        public static Config Config => _config ??= GetConfig();
+        public static System.Timers.Timer Interval = new System.Timers.Timer();
+        public static int ?Id;
 
         public static Task Main(string[] args) => new Program().MainAsync();
         public async Task MainAsync()
         {
-            if (await Config.ReadConfigs())
+            using (ServiceProvider services = ConfigureServices())
             {
-                using (ServiceProvider services = ConfigureServices())
-                {
-                    _client = services.GetRequiredService<DiscordSocketClient>();
+                _client = services.GetRequiredService<DiscordSocketClient>();
 
-                    await services.GetRequiredService<TimedEvent>().InitializeAsync();
-                    await services.GetRequiredService<InteractionCreated>().InitializeAsync();
-                    await services.GetRequiredService<Ready>().InitializeAsync();
+                await services.GetRequiredService<InteractionCreated>().InitializeAsync();
+                await services.GetRequiredService<Ready>().InitializeAsync();
+                await services.GetRequiredService<TimedEvent>().InitializeAsync();
 
-                    await _client.LoginAsync(TokenType.Bot, Config.Token);
-                    await _client.StartAsync();
+                await _client.LoginAsync(TokenType.Bot, Config.Token);
+                await _client.StartAsync();
 
-                    await Task.Delay(-1);
-                };
-            }
-            else
-            {
-                Logs.Log("Config files created!");
+                await Task.Delay(-1);
             };
         }
         private ServiceProvider ConfigureServices()
@@ -51,10 +47,29 @@ namespace BugReportBot
              .AddSingleton(_socketConfig)
             .AddSingleton<DiscordSocketClient>()
             .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
-            .AddSingleton<TimedEvent>()
             .AddSingleton<InteractionCreated>()
             .AddSingleton<Ready>()
+            .AddSingleton<TimedEvent>()
             .BuildServiceProvider();
+        }
+
+        private static Config GetConfig()
+        {
+            string _path = Path.Combine(Environment.CurrentDirectory, @"Configs", "config.json");
+            if (File.Exists(_path))
+            {
+                return JsonConvert.DeserializeObject<Config>(File.ReadAllText(_path))!;
+            };
+
+            if (!Directory.Exists(Path.Combine(Environment.CurrentDirectory, @"Configs")))
+            {
+                Directory.CreateDirectory(Path.Combine(Environment.CurrentDirectory, @"Configs"));
+            };
+
+            File.WriteAllText(_path, JsonConvert.SerializeObject(Config.Default, Formatting.Indented));
+            Log.Info("Config files created!");
+            Environment.Exit(1);
+            return Config.Default;
         }
     }
 }
